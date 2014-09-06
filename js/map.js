a442413f6ac95b409625ff3e9737821b16a67ef5
felri885 <felri885@student.liu.se>
2014-09-06 10:29:31 +0200

    added map and css files

--- js/map.js
+++ js/map.js
@@ -1,46 +1,47 @@
-var map;
-var homeMarker;
-
-var myLatlng;
-var centerLatlng;
+var map; //the map
+var homeMarker; //marker in the users location
+var myLatlng; //user home location
+var centerLatlng; //location of Linkoping centrum
 
 
 function initialize() { 
   centerLatlng = new google.maps.LatLng(58.41075642311165, 15.621438785552755); //linkoping centrum
-  myLatlng = centerLatlng; //set home position to centrum by default
+  myLatlng = centerLatlng; //set user location to centrum by default
 
-
+  /* Map style */
   var styles = [
   {
-    stylers: [
-      { hue: "#4cd9c0" },
-      { saturation: 20 },
-      { lightness: 0  }
-    ]
-  },{
-    featureType: "road",
-    elementType: "geometry",
-    stylers: [
-      { lightness: 100 },
-      { visibility: "simplified" }
-    ]
-  },{
-    featureType: "road",
-    elementType: "labels",
-    stylers: [
-      { visibility: "on" }
-    ]
-  }
-];
+      stylers: [
+        { hue: "#4cd9c0" },
+        { saturation: 20 },
+        { lightness: 0  }
+      ]
+    },{
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        { lightness: 100 },
+        { visibility: "simplified" }
+      ]
+    },{
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        { visibility: "on" }
+      ]
+    }
+  ];
+  
   var mapOptions = {
     zoom: 13,
     center: centerLatlng,
     styles: styles
   }
 
-
+  /* Create the map */
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+  /* Home marker */
   homeMarker = new google.maps.Marker({
       map: map,
       draggable:true,
@@ -48,6 +49,7 @@ function initialize() {
       icon: 'img/home-blue.png'
   });
 
+  /* Use the users current location */
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude); // does not update the global variable, why???
@@ -60,24 +62,28 @@ function initialize() {
     handleNoGeolocation(false);
   }
 
-
+  /*Content displayed in homwmarker tooltip*/
   var contentString = '<div id="content">'+
     '<div id="bodyContent">'+
     '<p>Din bostad</p>'+
     '</div>'+
     '</div>';
 
+  /* setting content to tooltip/infowindow */
   var infowindow = new google.maps.InfoWindow({
       content: contentString
   });
 
+  /* Adding tooltip/infowindow to homeMarker, display when clicking the marker */
   google.maps.event.addListener(homeMarker, 'click', function() {
     infowindow.open(map, homeMarker);
   });
 
+  /* After moving the marker (on mouseup), run updatePos function */
   google.maps.event.addListener(homeMarker, 'mouseup', updatePos);
 }
 
+/* If geolocation fails - print console message */
 function handleNoGeolocation(errorFlag) {
   if (errorFlag) {
     console.log('Error: The Geolocation service failed.');
@@ -86,6 +92,7 @@ function handleNoGeolocation(errorFlag) {
   }
 }
 
+/* When moving the user location marker (homeMarker) the location variable is updated */
 function updatePos() {
   myLatlng = new google.maps.LatLng(homeMarker.getPosition().lat(), homeMarker.getPosition().lng());
 }
