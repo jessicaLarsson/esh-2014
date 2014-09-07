cb76b4ecd125b0e42a27502d322f3505fd011123
felri885 <felri885@student.liu.se>
2014-09-07 12:45:10 +0200

    custom colors to bar chart

--- js/controllers/compareSchoolsController.js
+++ js/controllers/compareSchoolsController.js
@@ -132,7 +132,7 @@ schoolApp.controller("CompareSchoolsCtrl", function ($scope, $http, $routeParams
      
          var y = d3.scale.ordinal()
              .domain(data)
-             .rangeBands([0, 120]);
+             .rangeBands([0, 100]);
 
 
         chart.selectAll("rect")
@@ -142,16 +142,12 @@ schoolApp.controller("CompareSchoolsCtrl", function ($scope, $http, $routeParams
             .attr("width", x)
             .attr("height", y.rangeBand());
 
-        /*chart.selectAll("text")
-            .data(data)
-            .enter().append("svg:text")
-            .attr("x", x)
-            .attr("y", function(d) { return y(d) + y.rangeBand() / 2; })
-            .attr("dx", -3) // padding-right
-            .attr("dy", ".35em") // vertical-align: middle
-            .attr("text-anchor", "end") // text-align: right
-            .attr("fill", "white")
-            .text(String);*/
+        colors = ["red", "blue", "green", "black"];
+
+    chart.selectAll("rect")
+      .style("fill", function(d, i){
+        console.log("i" + i);
+        return colors[i]});
 
     }
 
